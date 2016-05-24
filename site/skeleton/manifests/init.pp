6bc465488a013be7b00c4cdf60e2b4400196e404
venkat tumkunta <reddytkv@users.noreply.github.com>
2016-05-24 13:26:36 -0500

    Create init.pp
    in this commit, I created the bashrc file

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,18 @@
+class skeleton {
+
+  file { '/etc/skel' :
+    ensure => directory,
+  
+  }
+
+  file { '/etc/skel/.bashrc': 
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/skeleton/bashrc'
+  
+  }
+
+
+}
