a184bc3656a0a910b25752cd51479b3d1cc3130f
Andrew Brader <abrader@gmail.com>
2016-05-24 12:00:07 -0400

    Update site.pp
    Updated site.pp for clarification on Lab 7.3

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,12 +49,14 @@ node 'abrader.puppetlabs.vm' {
 #    content => "Don't mess with my cat while he is eating :p\n",
 #  }
 
-  host { 'testing host entry':
-    name => 'testing.puppetlabs.vm',
-    ip   => '127.0.0.1',
+  host { 'testing':
+    ensure => present,
+    name   => 'testing.puppetlabs.vm',
+    ip     => '127.0.0.1',
   }
 
-  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
+  exec { 'cowsay motd' :
+    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
     path    => '/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
   }
