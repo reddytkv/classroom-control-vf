51ffde4b7cc51e98dc7c6f001d3ba020fadfd514
venkat tumkunta <reddytkv@users.noreply.github.com>
2016-05-25 09:37:47 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,20 +1,20 @@
-class nginx {
-  package{ ['openssl','openssl-libs'] :
-    ensure  =>  '1.0.1e-51.el7_2.5',
-    before  => Package['nginx'],
-  }
+class nginx { 
+   package { [ 'openssl', 'openssl-libs' ] : 
+     ensure => '1.0.1e-51.el7_2.5', 
+     before => Package['nginx'], 
+   } 
+ 
 
-  file { 'nginx rpm' :
-    ensure  =>  file,
-    path    =>  '/opt/nginx-1.6.2-el7.centos.ngx.x86_64.rpm',
-    source  =>  'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-  }
-
-package { ''nginx'  :
-  ensure  =>  '1.6.2-e17.centos.ngx',
-  source  =>  '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-  provider  => rpm,
-  require => File['nginx rpm'],
-}
-
-}
+   file { 'nginx rpm' : 
+     ensure   => file, 
+     path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm', 
+     source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm', 
+   } 
+ 
+   package { 'nginx' : 
+     ensure   => '1.6.2-1.el7.centos.ngx', 
+     source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm', 
+     provider => rpm, 
+     require  => File['nginx rpm'], 
+   } 
+ } 
