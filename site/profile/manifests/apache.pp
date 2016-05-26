bf326a4183b9562831f09977a2628dffec1137d2
venkat tumkunta <reddytkv@users.noreply.github.com>
2016-05-26 15:39:03 -0500

    updated apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,5 +1,27 @@
 class profile::apache {
+
+  $wp_home = '/opt/wordpress'
+  $blog_name = 'blog'
+
+  host { "${blog_name}.puppetlabs.vm" :
+    ensure       => present,
+    host_aliases => [$blog_name],
+    ip           => $::ipaddress,
+  }
+
   include ::apache
-  
   include ::apache::mod::php
+  
+  
+  file { $wp_home :
+    ensure => directory,
+  }
+
+  apache::vhost { $::fqdn :
+    port    => '80',
+    docroot => $wp_home,
+    require => File[$wp_home],
+  }
+
+
 }
